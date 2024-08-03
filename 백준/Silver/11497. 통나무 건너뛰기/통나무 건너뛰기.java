472b23b220d4d336fe21b379901c39e3e601c6d1
0taetae <teayoung0812@naver.com>
2024-08-04 01:49:39 +0900

    [Silver I] Title: 통나무 건너뛰기, Time: 672 ms, Memory: 63708 KB -BaekjoonHub

--- 백준/Silver/11497. 통나무 건너뛰기/통나무 건너뛰기.java
+++ 백준/Silver/11497. 통나무 건너뛰기/통나무 건너뛰기.java
@@ -10,17 +10,19 @@ public class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(br.readLine());
+		
 		for(int i=0;i<T;i++) {
 			int N = Integer.parseInt(br.readLine());
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			ArrayList<Integer> log = new ArrayList<Integer>();
-			ArrayList<Integer> result = new ArrayList<Integer>();
+			ArrayList<Integer> log = new ArrayList<Integer>();  // 입력받은 통나무 높이
+			ArrayList<Integer> result = new ArrayList<Integer>();  // 난이도가 가장 낮은 통나무 배열
 			
 			for(int j=0;j<N;j++) {
 				log.add(Integer.parseInt(st.nextToken()));
 			}
 			Collections.sort(log);
 			
+			// 낮이도가 가장 낮은 통나무 배열을 만들기 위해 1 3 5 4 2와 같은 순으로 배치 
 			for(int j=0;j<N;j+=2) {
 				result.add(log.get(j));
 			}
@@ -33,7 +35,9 @@ public class Main {
 					result.add(log.get(j));
 				}
 			}
+			
 			int max = 0;
+			// 배열의 난이도 
 			for(int j=0;j<N-1;j++) {
 				max = Math.max(max, Math.abs(result.get(j+1)-result.get(j)));
 			}

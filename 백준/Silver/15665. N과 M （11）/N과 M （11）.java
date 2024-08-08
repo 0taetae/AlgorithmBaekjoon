c1bdf727ca5b3c824bf2f609c6675376d22122ee
0taetae <teayoung0812@naver.com>
2024-08-08 21:59:32 +0900

    [Silver II] Title: N과 M (11), Time: 400 ms, Memory: 37228 KB -BaekjoonHub

---
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.java
@@ -0,0 +1,48 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+	static int[] arr ;
+	static int[] num;
+	static int N;
+	static int M;
+	static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		M = sc.nextInt();
+		
+		arr = new int[N+1];
+		for(int i=1;i<=N;i++) {
+			arr[i] = sc.nextInt();
+		}
+		Arrays.sort(arr);
+		
+		num = new int[M];
+		
+		Comb(0);
+		System.out.println(sb);
+		
+
+	}
+	public static void Comb(int idx) {
+		if(idx==M) {
+			for(int i=0;i<M;i++) {
+				sb.append(num[i]).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		int before = 0;
+		for(int i=1;i<=N;i++) {
+			if(before !=arr[i]) {
+				num[idx]=arr[i];
+				before = arr[i];
+				Comb(idx+1);
+			}
+		}
+	}
+
+}

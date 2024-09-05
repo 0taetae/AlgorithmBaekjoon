76414e522f0bfd8bf00bd20d44b22ef3386f9d2e
0taetae <teayoung0812@naver.com>
2024-09-05 23:18:04 +0900

    [Unrated] Title: [모의 SW 역량테스트] 보물상자 비밀번호, Time: 209 ms, Memory: 22,208 KB -BaekjoonHub

--- SWEA/Unrated/5658. ［모의 SW 역량테스트］ 보물상자 비밀번호/［모의 SW 역량테스트］ 보물상자 비밀번호.java
+++ SWEA/Unrated/5658. ［모의 SW 역량테스트］ 보물상자 비밀번호/［모의 SW 역량테스트］ 보물상자 비밀번호.java
@@ -1,60 +1,60 @@
 import java.io.*;
 import java.util.*;
-
+ 
 public class Solution {
-	
-	static int N,K;
-	static char[] box, copyBox;
-	static HashSet<String> set;
-	static ArrayList<Integer> lst;
-
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		StringBuilder sb = new StringBuilder();
-		int T = Integer.parseInt(br.readLine());
-		for(int tc=1; tc<=T;tc++) {
-			st = new StringTokenizer(br.readLine());
-			N = Integer.parseInt(st.nextToken());  // 숫자의 개수
-			K = Integer.parseInt(st.nextToken());  // 크기 순서 (K번째로 큰 수) 
-			String str = br.readLine();
-			box = new char[N];
-			copyBox = new char[N];  // 처음 상태와 비교할 배열 
-			set = new HashSet<String>();
-			lst = new ArrayList<Integer>();
-			for(int i=0;i<N;i++) {
-				box[i] = str.charAt(i);
-				copyBox[i] = str.charAt(i);
-			}
-			turn();
-			change();
-			Collections.sort(lst, new Comparator<Integer>() {
-
-				@Override
-				public int compare(Integer o1, Integer o2) {
-					return o2-o1;
-				}
-			});
-			sb.append("#").append(tc).append(" ").append(lst.get(K-1)).append("\n");
-		}
-		System.out.println(sb);
-	}
-	static void turn() {
-		while(true) {
-			
-			createNum();
-			char temp = box[N-1];
-			for(int i=N-1;i>=1;i--) {
-				box[i] = box[i-1];
-			}
-			box[0] = temp;
-			
-			if(Arrays.equals(box, copyBox)) {
-				break;
-			}
-		}
-	}
-	static void createNum() {
+     
+    static int N,K;
+    static char[] box, copyBox;
+    static HashSet<String> set;
+    static ArrayList<Integer> lst;
+ 
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+        for(int tc=1; tc<=T;tc++) {
+            st = new StringTokenizer(br.readLine());
+            N = Integer.parseInt(st.nextToken());  // 숫자의 개수
+            K = Integer.parseInt(st.nextToken());  // 크기 순서 (K번째로 큰 수) 
+            String str = br.readLine();
+            box = new char[N];
+            copyBox = new char[N];  // 처음 상태와 비교할 배열 
+            set = new HashSet<String>();
+            lst = new ArrayList<Integer>();
+            for(int i=0;i<N;i++) {
+                box[i] = str.charAt(i);
+                copyBox[i] = str.charAt(i);
+            }
+            turn();
+            change();
+            Collections.sort(lst, new Comparator<Integer>() {
+ 
+                @Override
+                public int compare(Integer o1, Integer o2) {
+                    return o2-o1;
+                }
+            });
+            sb.append("#").append(tc).append(" ").append(lst.get(K-1)).append("\n");
+        }
+        System.out.println(sb);
+    }
+    static void turn() {
+        while(true) {
+             
+            createNum();
+            char temp = box[N-1];
+            for(int i=N-1;i>=1;i--) {
+                box[i] = box[i-1];
+            }
+            box[0] = temp;
+             
+            if(Arrays.equals(box, copyBox)) {
+                break;
+            }
+        }
+    }
+    static void createNum() {
         for (int i = 0; i < N; i += N/4) {
             String num = "";
             for (int j = 0; j < N/4; j++) {
@@ -62,11 +62,11 @@ public class Solution {
             }
             set.add(num);
         }
-	}
-	static void change() {
-		for(String num : set) {
-			lst.add(Integer.parseInt(num,16));
-		}
-	}
-
+    }
+    static void change() {
+        for(String num : set) {
+            lst.add(Integer.parseInt(num,16));
+        }
+    }
+ 
 }

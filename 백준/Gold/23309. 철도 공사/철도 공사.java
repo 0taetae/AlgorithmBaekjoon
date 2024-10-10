acd8c69f54377a31555cec25b680475a03e838cb
0taetae <teayoung0812@naver.com>
2024-10-10 11:44:35 +0900

    [Gold IV] Title: 철도 공사, Time: 1516 ms, Memory: 316556 KB -BaekjoonHub

--- 백준/Gold/23309. 철도 공사/철도 공사.java
+++ 백준/Gold/23309. 철도 공사/철도 공사.java
@@ -4,22 +4,10 @@ import java.util.*;
 public class Main {
 
     static int N, M;
-    static Node head;
-    static Node[] arr = new Node[1000001];
-    //static Map<Integer, Node> map = new HashMap<>();
+    static int[] preLst = new int[1000001];
+    static int[] nextLst = new int[1000001];
+    static int head;
     static BufferedWriter bw; 
-
-    static class Node {
-        private int num;
-        public Node pre;
-        public Node next;
-
-        public Node(int num) {
-            this.num = num;
-            this.pre = null;
-            this.next = null;
-        }
-    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -32,27 +20,18 @@ public class Main {
 
         st = new StringTokenizer(br.readLine());
         
-        for(int i=0;i<1000001;i++) {
-        	arr[i] = new Node(i);
+        int pre = Integer.parseInt(st.nextToken());
+        nextLst[pre] = pre;
+        preLst[pre] = pre;
+        head = pre;
+        for (int i = 1; i < N; i++) {
+        	int next = Integer.parseInt(st.nextToken());
+            nextLst[pre] = next;
+            preLst[next] = pre;
+            pre = next;
         }
-        
-        Node preNode = null;
-        
-        for (int i = 0; i < N; i++) {
-        	int num = Integer.parseInt(st.nextToken());
-            //Node newNode = new Node(num);
-            //arr[num] = newNode;
-            //map.put(newNode.num, newNode);
-            if (head == null) {
-                head = arr[num];
-            } else {
-                preNode.next = arr[num];
-                arr[num].pre = preNode;
-            }
-            preNode = arr[num];
-        }
-        preNode.next = head; 
-        head.pre = preNode;
+        nextLst[pre] = head;
+        preLst[head] = pre;
 
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
@@ -82,74 +61,38 @@ public class Main {
     }
 
     // 고유 번호 a를 가진 역의 다음 역의 고유 번호를 출력하고, 그 사이에 고유 번호 b인 역을 설립
-    public static void bn(int a, int b) throws IOException {
-        Node cur = arr[a];
-        bw.write(cur.next.num + "\n");  
+    public static void bn(int a, int b) throws IOException {  // a b next
+        bw.write(nextLst[a] + "\n");  
 
-        Node newNode = new Node(b);
-        //map.put(b, newNode);
-        arr[b] = newNode;
-        Node nextNode = cur.next;
-
-        arr[b].pre = cur;
-        arr[b].next = nextNode;
-        nextNode.pre = arr[b];
-        cur.next = arr[b];
+        preLst[b] = a;  // b의 이전 역은 a
+        nextLst[b] = nextLst[a];  // b의 다음 역은 a의 다음 역
+        preLst[nextLst[a]] = b;  // a의 다음 역의 이전역은 b
+        nextLst[a] = b;  // a의 다음역은 b
     }
 
     // 고유 번호 a를 가진 역의 이전 역의 고유 번호를 출력하고, 그 사이에 고유 번호 b인 역을 설립
-    private static void bp(int a, int b) throws IOException {
-        Node cur = arr[a];
-        bw.write(cur.pre.num + "\n"); 
-
-        Node newNode = new Node(b);
-        //map.put(b, newNode);
-        arr[b] = newNode;
-        Node preNode = cur.pre;
-
-        arr[b].pre = preNode;
-        arr[b].next = cur;
-        preNode.next = arr[b];
-        cur.pre = arr[b];
+    private static void bp(int a, int b) throws IOException {  // pre b a
+        bw.write(preLst[a] + "\n"); 
+        
+        preLst[b] = preLst[a]; // b의 이전 역은 a의 이전 역
+        nextLst[b] = a;  // b의 다음 역은 a
+        nextLst[preLst[a]] = b;  // a의 이전역의 다음역은 b
+        preLst[a] = b;  // a의 이전 역은 b
     }
 
     // 고유 번호 a를 가진 역의 다음 역을 폐쇄하고 그 역의 고유 번호를 출력
-    private static void cn(int a) throws IOException {
-        Node cur = arr[a];
-        bw.write(cur.next.num + "\n"); 
-
-        Node nextNode = cur.next.next;
-
-        //map.remove(cur.next.num); 
-        arr[cur.next.num] = null;
-
-        cur.next = nextNode; 
-        if (nextNode != null) {
-            nextNode.pre = cur;
-        }
-
-        if (cur.next == head) {
-            head = nextNode;
-        }
+    private static void cn(int a) throws IOException {  // a next next.next -> a next.next
+        bw.write(nextLst[a] + "\n");
+        
+        preLst[nextLst[nextLst[a]]] = a;
+        nextLst[a] = nextLst[nextLst[a]];
     }
 
     // 고유 번호 a를 가진 역의 이전 역을 폐쇄하고 그 역의 고유 번호를 출력
-    private static void cp(int a) throws IOException {
-        Node cur = arr[a];
-        bw.write(cur.pre.num + "\n");  
+    private static void cp(int a) throws IOException {  // pre.pre pre a -> pre.pre a
+        bw.write(preLst[a] + "\n");  
 
-        Node preNode = cur.pre.pre; 
-
-        //map.remove(cur.pre.num); 
-        arr[cur.pre.num] = null;
-
-        if (preNode != null) { 
-            preNode.next = cur; 
-        }
-        cur.pre = preNode; 
-
-        if (cur.pre == head) { 
-            head = cur; 
-        }
+        nextLst[preLst[preLst[a]]] = a;
+        preLst[a] = preLst[preLst[a]];
     }
 }

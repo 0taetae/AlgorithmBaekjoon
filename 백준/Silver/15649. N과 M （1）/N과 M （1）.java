56270558588e79c335e1e97fc792d6afbcc07b18
0taetae <teayoung0812@naver.com>
2024-10-30 17:10:57 +0900

    [Silver III] Title: N과 M (1), Time: 1612 ms, Memory: 52192 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,37 +1,43 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	static int N;
-	static int M;
-	static boolean[] visit;
-	static int[] num;
 	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		N = sc.nextInt();
-		M = sc.nextInt();
-		
-		num = new int[M];
-		visit = new boolean[N+1];
-		
-		comb(0);
+	static int N,M;
+	static boolean[] visited;
+	static int[] select;
 
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		visited = new boolean[N+1];
+		select = new int[M];
+		
+		
+		perm(0);
 	}
-	static void comb(int idx) {
-		if(idx==M) {
-			for(int i=0;i<M;i++) {
-				System.out.print(num[i]+" ");
+
+	private static void perm(int cnt) {
+		if(cnt == M) {
+			for(int j=0;j<M;j++) {
+				System.out.print(select[j]+" ");
 			}
 			System.out.println();
 			return;
 		}
+		
 		for(int i=1;i<=N;i++) {
-			if(!visit[i]) {
-				visit[i]=true;
-				num[idx]=i;
-				comb(idx+1);
-				visit[i]=false;
+			if(!visited[i]) {
+				select[cnt]=i;
+				visited[i] = true;
+				perm(cnt+1);
+				visited[i] = false;
 			}
 		}
 	}
+
 }

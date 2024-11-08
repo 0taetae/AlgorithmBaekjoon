7333b89478274dfd231a65f477daefed3a7916cf
0taetae <teayoung0812@naver.com>
2024-11-08 09:22:57 +0900

    [D4] Title: 무한로봇, Time: 134 ms, Memory: 21,912 KB -BaekjoonHub

--- SWEA/D4/14362. 무한로봇/무한로봇.java
+++ SWEA/D4/14362. 무한로봇/무한로봇.java
@@ -9,71 +9,54 @@ public class Solution {
 	
 	static int[] dx = {-1,0,1,0};
 	static int[] dy = {0,1,0,-1};
-	//  규칙이 있을 것 같다!!!!!
 	
-
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(br.readLine());
 		for(int tc=1;tc<=T;tc++) {
 			String C = br.readLine();
+			// 원점에서 시작
 			int x = 0;
 			int y = 0;
-			//int nx = 0;
-			//int ny = 0;
+			// 처음은 오른쪽 방향 
 			int dir=1;
-			int res = 0;
-			int cnt = 0;
-			int up = 0;
-			while(cnt<10000) {
+			int res = 0;  // R^2
+			while(true) {
 				int startDir = dir;
 				for (int i = 0; i < C.length(); i++) {
-	            	//int startX = x; 
-	                //int startY = y;
 	                switch (C.charAt(i)) {
+	                	// 바라보는 방향으로 1만큼 이동 
 	                    case 'S':
 	                        x += dx[dir];
 	                        y += dy[dir];
 	                        res = Math.max(res,x*x+y*y);
 	                        break;
+	                    // 왼쪽으로 90도 회전
 	                    case 'L':
 	                        dir = (dir - 1 + 4) % 4;
 	                        break;
+	                    // 오른쪽으로 90도 회전 
 	                    case 'R':
 	                        dir = (dir + 1) % 4;
 	                        break;
 	                }
-	                
-//	                cnt++;
-//	                if(res < x*x + y*y){
-//	                	res = x*x + y*y;
-//	                	up++;
-//	                }
-	                //res = Math.max(res, Math.sqrt(Math.pow((x), 2) + Math.pow((y), 2)));
 	            }
+				// 모든 명령어를 수행했을 때 다시 원점으로 돌아오는 경우 -> 작업이 무한히 반복되어도 동일 
 				if(x==0 && y==0) {
 					break;
 				}
+				// 모든 명령어를 수행했을 때 다시 원점으로 돌아오지 않으면서, "모든 명령 실행 시작전 방향 == 모든명령 실행 후 방향" 인 경우 
+				// => R=oo
 				if(startDir==dir) {
 					res = -1;
 					break;
 				}
-	            //System.out.println(res*2);
-	            //cnt++;
 			}
 			if(res==-1) {
 				System.out.println("#"+tc+" oo");
 			}else {
 				System.out.println("#"+tc+" "+res);
 			}
-
-            
-			
-			//System.out.println("------");
-			
 		}
-		
-
 	}
-
 }

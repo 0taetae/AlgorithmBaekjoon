5bfc9d89cbcb944f4aa96cb578189b83453b483d
0taetae <teayoung0812@naver.com>
2024-11-10 14:25:34 +0900

    [Gold IV] Title: 스도쿠, Time: 384 ms, Memory: 19056 KB -BaekjoonHub

--- 백준/Gold/2239. 스도쿠/스도쿠.java
+++ 백준/Gold/2239. 스도쿠/스도쿠.java
@@ -1,67 +1,75 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
-	
-	static int[][] board = new int[9][9];
-	static boolean[][] visitedRow = new boolean[9][10];  // 몇번째 행, 해당 숫자가 있는지
-	static boolean[][] visitedCol = new boolean[9][10];  // 몇번째 열, 해당 숫자가 있는지
-	static boolean[][][] visited33 = new boolean[3][3][10];  // 3*3 영역에 해당 숫자가 있는지 
+public class Main{
 
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		for(int i=0;i<9;i++) {
-			String str = br.readLine();
-			for(int j=0;j<9;j++) {
-				board[i][j] = str.charAt(j)-'0';
-				if (board[i][j] != 0) {
-                    visitedRow[i][board[i][j]] = true;
-                    visitedCol[j][board[i][j]] = true;
-                    visited33[i / 3][j / 3][board[i][j]] = true;
+    static int[][] map = new int[9][9];
+    static boolean[][] visitedR = new boolean[9][10]; // 행 방문 배열
+    static boolean[][] visitedC = new boolean[9][10]; // 열 방문 배열
+    static boolean[][][] visited33 = new boolean[3][3][10]; // 3*3 방문 배열
+    static boolean isFinished = false;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // 맵 입력
+        for (int r = 0; r < 9; r++) {
+            String str = br.readLine();
+            for (int c = 0; c < 9; c++) {
+                map[r][c] = str.charAt(c) - '0';
+                if (map[r][c] != 0) {
+                    visitedR[r][map[r][c]] = true;
+                    visitedC[c][map[r][c]] = true;
+                    visited33[r/3][c/3][map[r][c]] = true;
                 }
-			}
-		}
-		
-		check(0,0);
-		
-	}
-	// 해당 열, 행에 없는 숫자 하나씩 넣어봄 -> 백트래킹 
-	static void check(int r, int c) {
-		if(c==9) {
-			r++;
-			c=0;
-		}
-		if(r==9) {
+            }
+        }
+
+        dfs(0, 0);
+    }
+
+    public static void dfs(int r, int c) {
+        if (isFinished) {
+            return;
+        }
+
+        if (c >= 9) {
+            r++;
+            c = 0;
+        }
+
+        if (r >= 9) {
+            isFinished = true;
             for (int i = 0; i < 9; i++) {
                 for (int j = 0; j < 9; j++) {
-                    System.out.print(board[i][j]);
+                    System.out.print(map[i][j]);
                 }
                 System.out.println();
             }
-            System.exit(0);
-		}
-		if(board[r][c]==0) {
-			for(int i=1;i<=9;i++) {
-				if(!visitedRow[r][i] && !visitedCol[c][i] && !visited33[r/3][c/3][i]) {
-					board[r][c] = i;
-					visitedRow[r][i] = true;
-					visitedCol[c][i] = true;
-					visited33[r/3][c/3][i] = true;
-					
-					check(r,c+1);
-					
-					board[r][c] = 0;
-					visitedRow[r][i] = false;
-					visitedCol[c][i] = false;
-					visited33[r/3][c/3][i] = false;
-					
-				}
-			}
-		}else {
-			check(r,c+1);
-		}
-		
-	}
+            return;
+        }
 
+        // 숫자가 채워지지 않은 칸
+        if (map[r][c] == 0) {
+            for (int i = 1; i <= 9; i++) {
+                if (!visitedR[r][i] && !visitedC[c][i] && !visited33[r/3][c/3][i]) {
+                    map[r][c] = i;
+                    visitedR[r][i] = true;
+                    visitedC[c][i] = true;
+                    visited33[r/3][c/3][i] = true;
+
+                    dfs(r, c + 1);
+
+                    if (!isFinished) {
+                        map[r][c] = 0;
+                        visitedR[r][i] = false;
+                        visitedC[c][i] = false;
+                        visited33[r/3][c/3][i] = false;
+                    }
+                }
+            }
+        } else {
+            dfs(r, c + 1);
+        }
+    }
 }

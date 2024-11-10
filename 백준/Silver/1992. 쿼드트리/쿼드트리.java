3298848c437b2c712193df29ba25767a6c2138c0
0taetae <teayoung0812@naver.com>
2024-11-10 15:42:03 +0900

    [Silver I] Title: 쿼드트리, Time: 108 ms, Memory: 14664 KB -BaekjoonHub

--- 백준/Silver/1992. 쿼드트리/쿼드트리.java
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.java
@@ -1,60 +1,54 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
+	
+	static int N;
+	static int[][] map;
 	static StringBuilder sb = new StringBuilder();
-	static char[][] quadtree;
+	
+	// 0은 white, 1은 black
 
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		quadtree = new char[N][N];
 		
-		for(int i=0;i<N;i++) {
+		
+		N = Integer.parseInt(br.readLine());
+		map = new int[N][N];
+		for(int r=0;r<N;r++) {
 			String str = br.readLine();
-			for(int j=0;j<N;j++) {
-				quadtree[i][j] = str.charAt(j);
+			for(int c=0;c<N;c++) {
+				map[r][c] = str.charAt(c) - '0';
 			}
 		}
 		
-		part(0,0,N);
+		check(0,0,N);
 		System.out.println(sb);
+		
+
 	}
 	
-	// 해당 영역이 모두 같은 문자 인지 확인
-	static boolean check(int row, int col, int size) {
-		for(int i=row;i<row+size;i++) {
-			for(int j=col;j<col+size;j++) {
-				if(quadtree[row][col]!=quadtree[i][j]) {
-					return false;
-				}
+	public static void check(int r, int c, int size) {
+		int sum =0;
+		for(int i=r;i<r+size;i++) {
+			for(int j=c;j<c+size;j++) {
+				sum+=map[i][j];
 			}
 		}
-		return true;
-		
-	}
-	static void part(int row, int col, int size) {
-		// 문자 같으면 해당 문자 넣기
-		if(check(row, col, size)) {
-			if(quadtree[row][col]=='0') {
-				sb.append('0');
-			}
-			else {
-				sb.append('1');
-			}
-		}
-		// 문자가 다르면 4분할, 해당 영역의 문자가 같은지 확인하고 다르면 분할하는 과정을 반복 
-		else {
+		if(sum==0) {
+			sb.append(0);
+			return;
+		}else if(sum==size*size) {
+			sb.append(1);
+			return;
+		}else {
 			sb.append('(');
-			for(int i=row; i<row+size; i+=(size/2)) {
-				for(int j=col; j<col+size; j+=(size/2)) {
-					part(i, j, size/2);
-				}
-			}
+			check(r,c,size/2);
+			check(r,c+size/2, size/2);
+			check(r+size/2,c,size/2);
+			check(r+size/2,c+size/2,size/2);
 			sb.append(')');
-			
 		}
 	}
+
 }

e197a0ee38bcc9a8bf273424bb947e1d3d22b1b1
0taetae <teayoung0812@naver.com>
2024-11-11 17:27:15 +0900

    [Unrated] Title: [모의 SW 역량테스트] 요리사, Time: 277 ms, Memory: 22,360 KB -BaekjoonHub

--- SWEA/Unrated/4012. ［모의 SW 역량테스트］ 요리사/［모의 SW 역량테스트］ 요리사.java
+++ SWEA/Unrated/4012. ［모의 SW 역량테스트］ 요리사/［모의 SW 역량테스트］ 요리사.java
@@ -1,66 +1,60 @@
-
-
-import java.util.StringTokenizer;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
+import java.io.*;
+import java.util.*;
 
 public class Solution {
 	
-	static int[][] arr;
-	static boolean[] visit;
 	static int N;
-	static int min;
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		//BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringBuilder sb = new StringBuilder();
-		int T = Integer.parseInt(br.readLine());
-		for(int i=1; i<=T; i++) {
-			N = Integer.parseInt(br.readLine());
-			arr = new int[N][N];
-			visit = new boolean[N];
-			min = Integer.MAX_VALUE;
-			
-			for(int r=0; r<N; r++) {
-				StringTokenizer st = new StringTokenizer(br.readLine());
-				for(int c=0; c<N; c++) {
-					arr[r][c] = Integer.parseInt(st.nextToken());
-				}
-			}
-			comb(0,0);
-			
-			sb.append("#").append(i).append(" ").append(min).append("\n");
-		}
-		System.out.println(sb);
-	}   
+	static int[][] map;
+	static boolean[] selected;
+	static int res;
 
-	
-	static void comb(int start, int cnt) {
-		if(cnt==N/2) {
-			int x=0;
-			int y=0;
-			for(int i=0; i<N; i++) {
-				for(int j=0; j<N; j++) {
-					if(i==j) continue;
-					if(visit[i] && visit[j]) {
-						x+=arr[i][j];
-					}
-					else if(!visit[i] && !visit[j]){
-						y+=arr[i][j];
-					}
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		for(int tc=1;tc<=T;tc++) {
+			N = Integer.parseInt(br.readLine());
+			map = new int[N+1][N+1];
+			for(int r=1;r<=N;r++) {
+				StringTokenizer st = new StringTokenizer(br.readLine());
+				for(int c=1;c<=N;c++) {
+					map[r][c] = Integer.parseInt(st.nextToken());
 				}
 			}
-			min = Math.min(min,Math.abs(x-y));
-			return;
+			selected = new boolean[N+1];
+			res = Integer.MAX_VALUE;
+			
+			comb(1,0);
+			System.out.println("#"+tc+" "+res);
+			
 		}
 		
-		for(int i=start; i< N; i++) {
-			visit[i]=true;
-			comb(i+1,cnt+1);
-			visit[i]=false;
+		
+	}
+	public static void comb(int idx, int cnt) {
+		if(cnt==N/2) {
+			int sumA=0;
+			int sumB=0;
+			for(int r=1;r<=N;r++) {
+				
+				for(int c=1;c<=N;c++) {
+					if(selected[r] &&selected[c]) {
+						sumA+=map[r][c];
+					}
+					if(!selected[r] && !selected[c]) {
+						sumB+=map[r][c];
+					}
+				}
+			}
+			res = Math.min(res,Math.abs(sumA-sumB));
+			return;
+		}
+		for(int i=idx;i<=N;i++) {
+			if(!selected[i]) {
+				selected[i] = true;
+				comb(i+1,cnt+1);
+				selected[i] = false;
+			}
 		}
 	}
+
 }

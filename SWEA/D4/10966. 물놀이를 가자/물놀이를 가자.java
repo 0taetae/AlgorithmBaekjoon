6e421439ac220175b42b97c78b48aa20b5043d63
0taetae <teayoung0812@naver.com>
2024-11-12 21:58:54 +0900

    [D4] Title: 물놀이를 가자, Time: 1,290 ms, Memory: 122,836 KB -BaekjoonHub

--- SWEA/D4/10966. 물놀이를 가자/물놀이를 가자.java
+++ SWEA/D4/10966. 물놀이를 가자/물놀이를 가자.java
@@ -2,82 +2,74 @@ import java.io.*;
 import java.util.*;
 
 public class Solution {
-
-	static int N, M;
+	
+	static int N,M;
 	static char[][] map;
-	static int[][] dist;
-	static int[] dx = { -1, 1, 0, 0 };
-	static int[] dy = { 0, 0, -1, 1 };
-
-	static Queue<Info> water = new LinkedList<>();
-
-	static class Info {
-		int x, y, cnt;
-
-		Info(int x, int y, int cnt) {
+	static Queue<Info> q ;
+	static class Info{
+		int x,y,cnt;
+		Info(int x, int y, int cnt){
 			this.x = x;
 			this.y = y;
 			this.cnt = cnt;
 		}
 	}
-	// 물 W, 땅 L
+	static int[][] dist;
+	static int[] dx = {-1,1,0,0};
+	static int[] dy = {0,0,-1,1};
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
 		int T = Integer.parseInt(br.readLine());
-		for (int tc = 1; tc <= T; tc++) {
+		StringTokenizer st;
+		
+		for(int tc=1;tc<=T;tc++) {
 			st = new StringTokenizer(br.readLine());
 			N = Integer.parseInt(st.nextToken());
 			M = Integer.parseInt(st.nextToken());
 			map = new char[N][M];
 			dist = new int[N][M];
-
-			for (int r = 0; r < N; r++) {
+			q = new LinkedList<>();
+			for(int r=0;r<N;r++) {
 				String str = br.readLine();
-				for (int c = 0; c < M; c++) {
+				for(int c=0;c<M;c++) {
 					map[r][c] = str.charAt(c);
-					if (map[r][c] == 'W') {
-						water.add(new Info(r, c,0));
-					} else {
+					if(map[r][c]=='W') {
+						q.add(new Info(r,c,0));
+					}else {
 						dist[r][c] = Integer.MAX_VALUE;
 					}
 				}
 			}
-			int res = 0;
 			bfs();
-			for (int r = 0; r < N; r++) {
-				for (int c = 0; c < M; c++) {
-					if (map[r][c] == 'L') {
-						res += dist[r][c];
-					}
+			int res=0;
+			for(int r=0;r<N;r++) {
+				for(int c=0;c<M;c++) {
+					res+=dist[r][c];
 				}
 			}
-			System.out.println("#" + tc + " " + res);
-
+			System.out.println("#"+tc+" "+res);
 		}
+
 	}
-
-	private static int bfs() {
-
-		while (!water.isEmpty()) {
-			Info cur = water.poll();
-			for (int dir = 0; dir < 4; dir++) {
+	public static void bfs() {
+		while(!q.isEmpty()) {
+			Info cur = q.poll();
+			
+			for(int dir=0;dir<4;dir++) {
 				int nx = cur.x + dx[dir];
 				int ny = cur.y + dy[dir];
-
-				if (nx < 0 || ny < 0 || nx >= N || ny >= M)
-					continue;
-
-				if (dist[nx][ny] > cur.cnt + 1) {
+				
+				if(nx<0 || ny<0 || nx>=N || ny>=M) continue;
+				
+				if(dist[nx][ny]>cur.cnt+1) {
 					dist[nx][ny] = cur.cnt + 1;
-					water.add(new Info(nx, ny, cur.cnt + 1));
+					q.add(new Info(nx,ny,cur.cnt+1));
 				}
-
 			}
+			
+			
 		}
-
-		return 0;
 	}
 
 }

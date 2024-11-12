528ce630460f8b58402258852745f31a0a0736b9
0taetae <teayoung0812@naver.com>
2024-11-12 10:16:13 +0900

    [D4] Title: [Professional] 키 순서, Time: 2,047 ms, Memory: 92,160 KB -BaekjoonHub

--- SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
+++ SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
@@ -1,61 +1,56 @@
 import java.io.*;
 import java.util.*;
 
+// 다익스트라 알고리즘 
 public class Solution {
 	
 	static int N,M;
 	static boolean[][] Short;
 	static boolean[][] Long;
 
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
 		int T = Integer.parseInt(br.readLine());
 		for(int tc=1;tc<=T;tc++) {
-			N = Integer.parseInt(br.readLine());  // 학생들의 수
-			M = Integer.parseInt(br.readLine());  // 두 학생 키를 비교한 횟수
-			
-			Short = new boolean[N+1][N+1];  // 자신 보다 작은 사람 
-			Long = new boolean[N+1][N+1];  // 자신 보다 큰 사람
+			N = Integer.parseInt(br.readLine());
+			M = Integer.parseInt(br.readLine());
+			Short = new boolean[N+1][N+1];
+			Long  = new boolean[N+1][N+1];
 			for(int i=0;i<M;i++) {
 				StringTokenizer st = new StringTokenizer(br.readLine());
-				int a = Integer.parseInt(st.nextToken());  // 작은 사람
-				int b = Integer.parseInt(st.nextToken());  // 큰 사람
-				Long[a][b] = true;
+				int a = Integer.parseInt(st.nextToken());
+				int b = Integer.parseInt(st.nextToken());
 				Short[b][a] = true;
+				Long[a][b] = true;
 			}
-			
-			// 플로이드 워셜 알고리즘으로 자신보다 작은 사람, 큰 사람 모두 구하기 
-			for(int k=1;k<=N;k++) {
-				for(int i=1;i<=N;i++) {
-					for(int j=1;j<=N;j++) {
-						if(Long[i][k] && Long[k][j]) {
-							Long[i][j] = true;
-						}
-						if(Short[i][k] && Short[k][j]) {
-							Short[i][j] = true;
-						}
-					}
-				}
-			}
-			int res=0;
+			int res = 0;
 			for(int i=1;i<=N;i++) {
-				int sum=0;
-				for(int j=1;j<=N;j++) {
-					if(Long[i][j]) {
-						sum++;
-					}
-					if(Short[i][j]) {
-						sum++;
-					}
-				}
-				if(sum==N-1) {
+				if(check(i,Long) + check(i,Short)==N-1) {
 					res++;
 				}
 			}
 			System.out.println("#"+tc+" "+res);
 		}
 
+	}
+
+	private static int check(int start, boolean[][] target) {
+		Queue<Integer> q = new LinkedList<>();
+		boolean[] visited = new boolean[N+1];
+		q.add(start);
+		visited[start] = true;
+		int sum=0;
+		while(!q.isEmpty()) {
+			int cur = q.poll();
+			for(int i=1;i<=N;i++) {
+				if(!visited[i] && target[cur][i]) {
+					visited[i] = true;
+					q.add(i);
+					sum++;
+				}
+			}
+		}
+		return sum;
 	}
 
 }

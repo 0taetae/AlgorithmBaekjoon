36511f2877b335811478227bc86f5137362a2e97
0taetae <teayoung0812@naver.com>
2024-11-13 15:03:11 +0900

    [D6] Title: [S/W 문제해결 응용] 10일차 - 작업순서, Time: 162 ms, Memory: 30,032 KB -BaekjoonHub

--- SWEA/D6/1267. ［S／W 문제해결 응용］ 10일차 － 작업순서/［S／W 문제해결 응용］ 10일차 － 작업순서.java
+++ SWEA/D6/1267. ［S／W 문제해결 응용］ 10일차 － 작업순서/［S／W 문제해결 응용］ 10일차 － 작업순서.java
@@ -4,65 +4,57 @@ import java.util.*;
 public class Solution {
 	
 	static int V,E;
-	static int[] dep, cnt;
-	static ArrayList<Integer>[] list;
-	
-	
 
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
-		
 		for(int tc=1;tc<=10;tc++) {
 			st = new StringTokenizer(br.readLine());
 			V = Integer.parseInt(st.nextToken());
 			E = Integer.parseInt(st.nextToken());
-			dep = new int[V+1];
-			cnt = new int[V+1];
-			list = new ArrayList[V+1];
-			for(int i=0;i<=V;i++) {
-				list[i] = new ArrayList<>();
-			}
-			
+			int[] res = new int[V+1];  // 부모 표시 
 			st = new StringTokenizer(br.readLine());
+			ArrayList<Integer>[] child = new ArrayList[V+1];
+			ArrayList<Integer>[] parent = new ArrayList[V+1];
+			for(int i=0;i<=V;i++) {  // 자식들 담기
+				child[i] = new ArrayList<>();
+				parent[i] = new ArrayList<>();
+			}
 			for(int i=0;i<E;i++) {
 				int p = Integer.parseInt(st.nextToken());
 				int c = Integer.parseInt(st.nextToken());
-				list[p].add(c);  // 부모 노드 넣기
-				dep[c]++;
+				//parent[c] = p;
+				child[p].add(c);
+				parent[c].add(p);
 			}
 			Queue<Integer> q = new LinkedList<>();
 			for(int i=1;i<=V;i++) {
-				// 선행 작업큐에 담기 
-				if(dep[i]==0) {
+				if(parent[i].size()==0) {
 					q.add(i);
-					cnt[i] = 1;
 				}
 			}
 			
 			while(!q.isEmpty()) {
 				int cur = q.poll();
-				for(int j=0;j<list[cur].size();j++) {
-					int temp = list[cur].get(j);
-					dep[temp] --;
-					if(dep[temp]==0) {
-						q.add(temp);
-						cnt[temp] = cnt[cur]+1;
-					}
+				for(int i=0;i<child[cur].size();i++) {
+					int temp = child[cur].get(i);
+					q.add(temp);
+					res[temp] = res[cur]+1;
 				}
 			}
-			
-			System.out.print("#"+tc+" ");
-			for(int i=1;i<=V;i++) {
+			sb.append("#").append(tc).append(" ");
+			for(int i=0;i<=V;i++) {
 				for(int j=1;j<=V;j++) {
-					if(cnt[j]==i) {
-						System.out.print(j+" ");
+					if(res[j]==i) {
+						sb.append(j).append(" ");
 					}
 				}
 			}
-			System.out.println();
+			sb.append("\n");
+			
 		}
-		
+		System.out.println(sb);
 
 	}
 

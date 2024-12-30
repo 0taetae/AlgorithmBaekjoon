8562503fe2284367a00af031e34f92b71887c623
0taetae <teayoung0812@naver.com>
2024-12-30 20:50:51 +0900

    [Gold IV] Title: 부분합, Time: 252 ms, Memory: 24044 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.java
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -2,38 +2,42 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		
-		st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int S = Integer.parseInt(st.nextToken());
-		
-		int[] arr = new int[N];
-		
-		st = new StringTokenizer(br.readLine());
-		for(int i=0;i<N;i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
-		int sum=0;
-		int minCnt=Integer.MAX_VALUE;
-		for(int i=0;i<N;i++) {
-			sum=0;
-			for(int j=i;j<N;j++) {
-				sum+=arr[j];
-				
-				if(sum >=S) {
-					minCnt = Math.min(minCnt,j-i+1);
-					break;
-				}
-			}
-		}
-		if(minCnt == Integer.MAX_VALUE) {
-			System.out.println(0);
-		}else {
-			System.out.println(minCnt);
-		}
-		
-	}
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int S = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int start = 0;
+        int end = 0;
+        int sum = 0;
+        int minCnt = Integer.MAX_VALUE;
+
+        while (end < N) {
+            sum += arr[end];
+
+            while (sum >= S) {
+                minCnt = Math.min(minCnt, end - start+1);
+                sum -= arr[start];
+                start++;
+            }
+            end++;
+        }
+        if(minCnt==Integer.MAX_VALUE) {
+        	System.out.println(0);
+        }else {
+        	System.out.println(minCnt);
+        }
+    }
 }
+

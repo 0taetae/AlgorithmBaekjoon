aa4c32b9bd2fa588cadcbab50964f95efc3fe11c
0taetae <teayoung0812@naver.com>
2025-01-19 18:14:28 +0900

    Update 행렬 곱셈 순서.java

--- 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.java
+++ 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.java
@@ -27,18 +27,18 @@ public class Main {
 		}
 		
 		for (int len = 1; len < N; len++) {
-            for (int i = 0; i + len < N; i++) {
-                int j = i + len;
-                for (int k = i; k < j; k++) {
-        	    /* (행렬 i부터 행렬 k까지 곱하는데 드는 최소 비용) + (행렬 k+1부터 행렬 j까지 곱하는데 드는 최소 비용) 
-                         + 곱셈 비용(행렬 i ~ 행렬 k와 행렬 k+1 ~ 행렬 j를 곱할 때 비용)
-                       곱셈 비용 = 행렬 i의 행 개수 * 행렬 k의 열 개수 * 행렬 j의 열 개수 
-                    */
-                    int cost = dp[i][k] + dp[k + 1][j] + size[i][0] * size[k][1] * size[j][1]; 
-                    dp[i][j] = Math.min(dp[i][j], cost);
-                }
-            }
-        }
+            		for (int i = 0; i + len < N; i++) {
+                		int j = i + len;
+                		for (int k = i; k < j; k++) {
+        	    			/* (행렬 i부터 행렬 k까지 곱하는데 드는 최소 비용) + (행렬 k+1부터 행렬 j까지 곱하는데 드는 최소 비용) 
+                         		       + 곱셈 비용(행렬 i ~ 행렬 k와 행렬 k+1 ~ 행렬 j를 곱할 때 비용)
+                       			    곱셈 비용 = 행렬 i의 행 개수 * 행렬 k의 열 개수 * 행렬 j의 열 개수 
+                    			*/
+                    			int cost = dp[i][k] + dp[k + 1][j] + size[i][0] * size[k][1] * size[j][1]; 
+                    			dp[i][j] = Math.min(dp[i][j], cost);
+                		}
+            		}
+        	}
 		System.out.println(dp[0][N-1]);
 
 	}

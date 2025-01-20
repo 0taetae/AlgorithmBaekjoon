e757e2f2c593d89f30c3b58bc22a99317f4d66dd
0taetae <teayoung0812@naver.com>
2025-01-20 23:11:42 +0900

    [Silver V] Title: 막대기, Time: 104 ms, Memory: 14288 KB -BaekjoonHub

---
+++ 백준/Silver/1094. 막대기/막대기.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int X = Integer.parseInt(br.readLine());
+        
+        int cnt = 0;
+        
+        while (X > 0) {
+            if (X % 2 == 1) {
+                cnt++;
+            }
+            X /= 2;
+        }
+        
+        System.out.println(cnt);
+    }
+}

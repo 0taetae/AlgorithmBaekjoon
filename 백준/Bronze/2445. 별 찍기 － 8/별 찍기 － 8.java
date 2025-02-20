a078dc500e378350a54c6b94750de00ed382c7eb
0taetae <teayoung0812@naver.com>
2025-02-21 01:03:45 +0900

    [Bronze III] Title: 별 찍기 - 8, Time: 104 ms, Memory: 14280 KB -BaekjoonHub

---
+++ 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.java
@@ -0,0 +1,25 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 1; i <= n; i++) {
+            sb.append("*".repeat(i));
+            sb.append(" ".repeat(2 * (n - i)));
+            sb.append("*".repeat(i)).append("\n");
+        }
+
+        for (int i = n - 1; i >= 1; i--) {
+            sb.append("*".repeat(i));
+            sb.append(" ".repeat(2 * (n - i)));
+            sb.append("*".repeat(i)).append("\n");
+        }
+
+        System.out.print(sb);
+        br.close();
+    }
+}

c1dc9801398713cd2a63ead4e7b49e4a2b3af9e4
0taetae <teayoung0812@naver.com>
2025-03-01 09:29:06 +0900

    [Bronze V] Title: 대소문자 바꾸기, Time: 100 ms, Memory: 14168 KB -BaekjoonHub

---
+++ 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+        
+        StringBuilder result = new StringBuilder();
+        
+        for (char c : input.toCharArray()) {
+            if (Character.isUpperCase(c)) {
+                result.append(Character.toLowerCase(c));
+            } else {
+                result.append(Character.toUpperCase(c));
+            }
+        }
+        
+        System.out.println(result);
+    }
+}

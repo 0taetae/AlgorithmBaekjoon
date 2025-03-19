381926cedf4987dd759955313c64d385e842d374
0taetae <teayoung0812@naver.com>
2025-03-20 00:18:21 +0900

    [Bronze III] Title: 짝수를 찾아라, Time: 124 ms, Memory: 15136 KB -BaekjoonHub

---
+++ 백준/Bronze/3058. 짝수를 찾아라/짝수를 찾아라.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        
+        for (int t = 0; t < T; t++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int sum = 0;
+            int minEven = Integer.MAX_VALUE;
+            
+            for (int i = 0; i < 7; i++) {
+                int num = Integer.parseInt(st.nextToken());
+                if (num % 2 == 0) {
+                    sum += num;
+                    minEven = Math.min(minEven, num);
+                }
+            }
+            sb.append(sum).append(" ").append(minEven).append("\n");
+        }
+        
+        System.out.print(sb);
+    }
+}

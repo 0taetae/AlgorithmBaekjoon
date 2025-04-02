4b12dac48c2b75820479b7cde56092a2a9a8cea3
0taetae <teayoung0812@naver.com>
2025-04-02 23:17:45 +0900

    [Bronze IV] Title: 유치원생 파댕이 돌보기, Time: 108 ms, Memory: 14356 KB -BaekjoonHub

---
+++ 백준/Bronze/30979. 유치원생 파댕이 돌보기/유치원생 파댕이 돌보기.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim()); 
+        int N = Integer.parseInt(br.readLine().trim()); 
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int totalTime = 0;
+        
+        for (int i = 0; i < N; i++) {
+            totalTime += Integer.parseInt(st.nextToken());
+        }
+        
+        if (totalTime >= T) {
+            System.out.println("Padaeng_i Happy");
+        } else {
+            System.out.println("Padaeng_i Cry");
+        }
+    }
+}
